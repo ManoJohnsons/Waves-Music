3aab1504c3cdbf20b3d4c97213a2745a68216126
John Paulo <johnpaulo0401@gmail.com>
2023-10-28 15:55:50 -0300

    Adicionando recurso básicos

---
+++ js/songs.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        src: "./src/music/7 . FBC & VHOOR ft. Djair Voz Cristalina - Delírios.mp3",
+        name: "Delírios",
+        artist: "FBC & VHOOR feat Djair Voz Cristalina",
+        image: "./src/img/Delirios.jpg"
+    },
+    {
+        src: "./src/music/Dinero.mp3",
+        name: "Dinero",
+        artist: "Trinidad Cardona",
+        image: "./src/img/Dinero.jpg"
+    },
+    {
+        src: "./src/music/Heartbeat.mp3",
+        name: "Heartbeat",
+        artist: "Childish Gambino",
+        image: "./src/img/Heartbeat.jpg"
+    },
+    {
+        src: "./src/music/LET EM PRAY.mp3",
+        name: "LET EM PRAY",
+        artist: "NF",
+        image: "./src/img/Let em pray.jpg"
+    },
+    {
+        src: "./src/music/Locked out of Heaven.mp3",
+        name: "Locked out of Heaven",
+        artist: "Bruno Mars",
+        image: "./src/img/Locked out of heaven.jpg"
+    },
+    {
+        src: "./src/music/Promiscuous.mp3",
+        name: "Promiscuous",
+        artist: "Nelly Furtado feat Timbaland",
+        image: "./src/img/Promiscuous.jpg"
+    },
+    {
+        src: "./src/music/Show.mp3",
+        name: "Show",
+        artist: "Ado",
+        image: "./src/img/Show.jpg"
+    },
+    {
+        src: "./src/music/SPIT IN MY FACE!.mp3",
+        name: "SPIT IN MY FACE!",
+        artist: "ThxSoMch",
+        image: "./src/img/Spit in my face.jpg"
+    },
+    {
+        src: "./src/music/The Perfect Girl.mp3",
+        name: "The Perfect Girl",
+        artist: "Mareux",
+        image: "./src/img/The perfect girl.jpg"
+    },
+    {
+        src: "./src/music/YUMMI.mp3",
+        name: "YUMMI",
+        artist: "LXNGVX & YOUK3IV",
+        image: "./src/img/Yummi.jpg"
+    },
+];
